8a2790a10c72d6b34833464ddeb4bac4ddf655ef
Augusto Sell dos Santos <105236980+augustosellsantos@users.noreply.github.com>
2022-06-08 17:17:34 -0300

    finalizado desafio IMC

--- cursojs/seçao-3/ex019/assets/js/script.js
+++ cursojs/seçao-3/ex019/assets/js/script.js
@@ -8,36 +8,62 @@ function escopo () {
         const inputAltura = calculo.querySelector('#altura');
         const peso = Number(inputPeso.value);
         const altura = Number(inputAltura.value);
-        const imc = peso / (altura * altura);
+        
+        const imc = funImc(peso, altura);
+        const nivelImc = funNivelImc(imc);
+        const resposta = `Seu IMC é ${imc} (${nivelImc})`;
+        resultado(resposta, true);
 
-        if (imc < 18.5) {
-            resultado(`Seu IMC é ${imc} (Abaixo do peso)`);
-        } else if (imc >= 18.5 && imc <= 24.9) {
-            console.log(`Seu IMC é ${imc} (Peso normal)`);
-        } else if (imc >= 25 && imc <= 29.9) {
-            console.log(`Seu IMC é ${imc} (Sobrepeso)`);
-        } else if (imc >= 30 && imc <= 34.9) {
-            console.log(`Seu IMC é ${imc} (Obsidade grau 1)`);
-        } else if (imc >= 35 && imc <= 39.9) {
-            console.log(`Seu IMC é ${imc} (Obsidade grau 2)`);
-        } else if (imc >= 40) {
-            console.log(`Seu IMC é ${imc} (Obsidade grau 3)`);
-        }
         if (!peso) {
-            resultado(`Peso inválido`);
+            resultado(`Peso inválido`, false);
             return;
         }
         if (!altura) {
-            resultado(`Altura inválida`);
+            resultado(`Altura inválida`, false);
             return;
         }
     }
     calculo.addEventListener('submit', eventoImc);
-    
-    function resultado (resposta) {
+    function funNivelImc (imc) {
+        const nivel = ['Abaixo do peso', 'Peso normal', 'Sobrepeso', 'Obsidade grau 1', 'Obsidade grau 2', 'Obsidade grau 3'];
+        if (imc < 18.5) {
+            return nivel[0];
+        }
+        if (imc >= 18.5 && imc <= 24.9) {
+            return nivel[1];
+        }
+        if (imc >= 25 && imc <= 29.9) {
+            return nivel[2] ;
+        }
+        if (imc >= 30 && imc <= 34.9) {
+            return nivel[3];
+        }
+        if (imc >= 35 && imc <= 39.9) {
+            return nivel[4];
+        }
+        if (imc >= 40) {
+            return nivel[5];
+        }
+    }
+    function funImc (peso, altura) {
+        const imc = peso / (altura * altura);
+        return imc.toFixed(2);
+    }
+    function criaP () {
+        const p = document.createElement('p');
+        return p;
+    }
+    function resultado (resposta, valido) {
         const resultado = document.querySelector('#resultado');
         resultado.innerHTML = ``;
-        resultado.innerHTML += resposta;
+        const p = criaP();
+        if (valido) {
+            p.classList.add('paragrafo-resposta');
+        } else {
+            p.classList.add('invalido');
+        }
+        p.innerHTML = resposta;
+        resultado.appendChild(p);
     }
 }
 escopo();

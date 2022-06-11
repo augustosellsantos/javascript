c1d902c5afa8484bd94796be662586d8aef1dadb
Augusto Sell dos Santos <105236980+augustosellsantos@users.noreply.github.com>
2022-06-11 18:58:11 -0300

    switch/case

---
+++ cursojs/seçao-3/ex022/index.js
@@ -0,0 +1,36 @@
+const data = new Date();
+const diaSemana = data.getDay();
+const nomeDiaSemana = getNomediasemana(diaSemana);
+
+function getNomediasemana(diaSemana) {
+    let nomeDiaSemana;
+
+    switch (diaSemana) {
+        case 0:
+            nomeDiaSemana = 'Domingo';
+            return nomeDiaSemana;
+        case 1:
+            nomeDiaSemana = 'Segunda';
+            return nomeDiaSemana;
+        case 2:
+            nomeDiaSemana = 'Terça';
+            return nomeDiaSemana;
+        case 3:
+            nomeDiaSemana = 'Quarta';
+            return nomeDiaSemana;
+        case 4:
+            nomeDiaSemana = 'Quinta';
+            return nomeDiaSemana;
+        case 5:
+            nomeDiaSemana = 'Sexta';
+            return nomeDiaSemana;
+        case 6:
+            nomeDiaSemana = 'Sábado';
+            return nomeDiaSemana;
+        default:
+            nomeDiaSemana = '';
+            return nomeDiaSemana;
+    }
+}
+
+console.log(diaSemana, nomeDiaSemana);

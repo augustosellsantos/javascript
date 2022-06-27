36b0a4aac0f9fd7393b21a55dbfb910fd0c0823f
Augusto Sell dos Santos <105236980+augustosellsantos@users.noreply.github.com>
2022-06-27 16:27:44 -0300

    exercício com for

--- cursojs/seçao-3/ex026/assets/js/script.js
+++ cursojs/seçao-3/ex026/assets/js/script.js
@@ -1,5 +1,18 @@
-alert(7);
 function escopo () {
-    
+    const elementos = [
+        {tag: 'p', texto: 'Praticando JavaScript!'},
+        {tag: 'div', texto: 'Preciso praticar mais para saber desenvolver!'},
+        {tag: 'section', texto: 'Não posso ter preguiça e medo!'},
+        {tag: 'footer', texto: 'Vencer minhas dificuldades.'},
+    ];
+    const container = document.querySelector('.container');
+    const div = document.createElement('div');
+    for (let i = 0; i < elementos.length; i++){
+        let {tag, texto} = elementos[i];
+        let tagCriada = document.createElement(tag);
+        tagCriada.innerHTML = texto;
+        div.appendChild(tagCriada);
+    }
+    container.appendChild(div);
 }
 escopo();

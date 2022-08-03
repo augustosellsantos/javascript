111ce74c9203b858877e0c4a215988b316b30030
Augusto Sell dos Santos <105236980+augustosellsantos@users.noreply.github.com>
2022-08-03 19:07:59 -0300

    projeto verificador de idade

---
+++ cursojs-cursoemvideo/modulo-d/ex012/assets/js/script.js
@@ -0,0 +1,8 @@
+function escopo(){
+    const ano = document.querySelector('input#ano');
+    const masculino = document.querySelector('input#masculino');
+    const feminino = document.querySelector('input#feminino');
+    const resultado = document.querySelector('div#resultado');
+    
+}
+escopo();

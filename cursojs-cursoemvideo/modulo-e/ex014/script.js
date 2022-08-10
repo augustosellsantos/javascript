e4ccb4f652f2f7cb9a8d057f2255baa8dfa99a4e
Augusto Sell dos Santos <105236980+augustosellsantos@users.noreply.github.com>
2022-08-10 18:24:48 -0300

    praticando for

---
+++ cursojs-cursoemvideo/modulo-e/ex014/script.js
@@ -0,0 +1,7 @@
+function escopo() {
+    let teste = 7;
+    for (teste; teste <= 17; teste++) {
+        console.log(`Passo ${teste}.`)
+    }
+}
+escopo();
---
+++ cursojs-cursoemvideo/modulo-e/ex015/assets/js/script.js
@@ -0,0 +1,8 @@
+function escopo(){
+    const inicial = document.querySelector('input#inicial');
+    const final = document.querySelector('input#final');
+    const passo = document.querySelector('input#passo');
+    const resultado = document.querySelector('div#resultado');
+    
+}
+escopo();

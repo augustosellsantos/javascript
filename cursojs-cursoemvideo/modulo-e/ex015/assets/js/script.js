bd356b94222882b05880cbf6b346f22b47f16ceb
Augusto Sell dos Santos <105236980+augustosellsantos@users.noreply.github.com>
2022-08-11 20:13:22 -0300

    correção de código

--- cursojs-cursoemvideo/modulo-e/ex015/assets/js/script.js
+++ cursojs-cursoemvideo/modulo-e/ex015/assets/js/script.js
@@ -1,8 +1,14 @@
 function escopo(){
-    const inicial = document.querySelector('input#inicial');
-    const final = document.querySelector('input#final');
-    const passo = document.querySelector('input#passo');
-    const resultado = document.querySelector('div#resultado');
-    
+    const contar = document.querySelector('.contar');
+
+    function eventoContar (event) {
+        event.preventDefault();
+        const inicial = document.querySelector('input#inicial');
+        const final = document.querySelector('input#final');
+        const passo = document.querySelector('input#passo');
+        const resultado = document.querySelector('div#resultado');
+        
+    }
+    contar.addEventListener('submit', eventoContar);
 }
 escopo();

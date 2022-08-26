943158d795d6608cecd729efe4ae11a7b42be683
Augusto Sell dos Santos <105236980+augustosellsantos@users.noreply.github.com>
2022-08-26 17:46:00 -0300

    código alterado

--- cursojs-cursoemvideo/modulo-e/ex015/assets/js/script.js
+++ cursojs-cursoemvideo/modulo-e/ex015/assets/js/script.js
@@ -3,12 +3,36 @@ function escopo(){
 
     function eventoContar (event) {
         event.preventDefault();
-        const inicial = document.querySelector('input#inicial');
-        const final = document.querySelector('input#final');
-        const passo = document.querySelector('input#passo');
-        const resultado = document.querySelector('div#resultado');
-        
+        const inputInicio = contar.querySelector('#inicio');
+        const inputFinal = contar.querySelector('#final');
+        const inputPasso = contar.querySelector('#passo');
+        const inicio = Number(inputInicio.value);
+        const final = Number(inputFinal.value);
+        const passo = Number(inputPasso.value);
+
+        if (inputInicio.value.length === 0 || inputFinal.value.length === 0 || inputPasso.value.length) {
+            //alert('ERRO! Faltam dados.');
+            resultado.innerHTML = `Impossível contar!`
+        } else {
+            resultado.innerHTML = `Contando: <br>`;
+            // Contagem crescente!
+            if (inicio < final) {
+                for(let contador = inicio; contador <= final; contador += passo) {
+                    resultado.innerHTML += ` ${contador}`
+                }
+            // Contagem regressiva!    
+            } else {
+                for(let contador = inicio; contador >= final; contador -= passo) {
+                    resultado.innerHTML += ` ${contador}`
+                }
+            }
+            resultado.innerHTML += `Fim!`
+        }
     }
     contar.addEventListener('submit', eventoContar);
+    function resultado () {
+        const resultado = document.querySelector('#resultado');
+        resultado.innerHTML = ``;
+    }
 }
 escopo();
